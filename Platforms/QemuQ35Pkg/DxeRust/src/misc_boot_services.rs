4483869221804907f75e7e38dfb518ecbb200488
John Schock <joschock@microsoft.com>
2023-06-13 15:52:46 -0700

    Implement install_configuration_table

--- Platforms/QemuQ35Pkg/DxeRust/src/misc_boot_services.rs
+++ Platforms/QemuQ35Pkg/DxeRust/src/misc_boot_services.rs
@@ -1,7 +1,15 @@
-use core::{ffi::c_void, slice::from_raw_parts};
+use alloc::{boxed::Box, vec};
+use core::{
+  ffi::c_void,
+  slice::{from_raw_parts, from_raw_parts_mut},
+};
+use r_efi::{
+  efi::{Guid, Status},
+  eficall, eficall_abi,
+  system::{BootServices, ConfigurationTable},
+};
 
-use r_efi::{efi::Status, eficall, eficall_abi, system::BootServices};
-
+use crate::{allocator::EFI_RUNTIME_SERVICES_DATA_ALLOCATOR, events::EVENT_DB, systemtables::SYSTEM_TABLE};
 eficall! {fn calculate_crc32 (data: *mut c_void, data_size: usize, crc_32: *mut u32) -> Status {
   if data.is_null() || data_size == 0 || crc_32.is_null() {
     return r_efi::efi::Status::INVALID_PARAMETER;
@@ -15,6 +23,103 @@ eficall! {fn calculate_crc32 (data: *mut c_void, data_size: usize, crc_32: *mut 
   r_efi::efi::Status::SUCCESS
 }}
 
+pub fn core_install_configuration_table(vendor_guid: Guid, vendor_table: Option<&mut c_void>) -> Result<(), Status> {
+  let mut global_st = SYSTEM_TABLE.lock();
+  let efi_st = global_st.as_mut().expect("System table support not initialized");
+  let st = efi_st.as_mut();
+
+  //if a table is already present, reconstruct it from the pointer and length in the st.
+  let old_cfg_table = if st.configuration_table.is_null() {
+    assert_eq!(st.number_of_table_entries, 0);
+    None
+  } else {
+    let ct_slice_box = unsafe {
+      Box::from_raw_in(
+        from_raw_parts_mut(st.configuration_table, st.number_of_table_entries),
+        &EFI_RUNTIME_SERVICES_DATA_ALLOCATOR,
+      )
+    };
+    Some(ct_slice_box)
+  };
+
+  // construct the new table contents as a vector.
+  let new_table = match old_cfg_table {
+    Some(cfg_table) => {
+      // a configuration table list is already present.
+      let mut current_table = cfg_table.to_vec();
+      let existing_entry = current_table.iter_mut().find(|x| x.vendor_guid == vendor_guid);
+      if let Some(vendor_table) = vendor_table {
+        //vendor_table is some; we are adding or modifying an entry.
+        if let Some(entry) = existing_entry {
+          //entry exists, modify it.
+          entry.vendor_table = vendor_table;
+        } else {
+          //entry doesn't exist, add it.
+          current_table.push(ConfigurationTable { vendor_guid: vendor_guid, vendor_table: vendor_table });
+        }
+      } else {
+        //vendor_table is none; we are deleting an entry.
+        if let Some(_entry) = existing_entry {
+          //entry exists, we can delete it
+          current_table.retain(|x| x.vendor_guid != vendor_guid);
+        } else {
+          //entry does not exist, we can't delete it. We have to put the original box back
+          //in the config table so it doesn't get dropped though. Pointer should be the same
+          //so we should not need to recompute CRC.
+          st.configuration_table = Box::into_raw(cfg_table) as *mut ConfigurationTable;
+          return Err(r_efi::efi::Status::NOT_FOUND);
+        }
+      }
+      current_table
+    }
+    None => {
+      //config table list doesn't exist.
+      if let Some(table) = vendor_table {
+        // table is some, meaning we should create the list and add this as the new entry.
+        vec![ConfigurationTable { vendor_guid, vendor_table: table }]
+      } else {
+        //table is none, but can't delete a table entry in a list that doesn't exist.
+        //since the list doesn't exist, we can leave the (null) pointer in the st alone.
+        return Err(r_efi::efi::Status::NOT_FOUND);
+      }
+    }
+  };
+
+  if new_table.len() == 0 {
+    // if empty, just set config table ptr to null
+    st.number_of_table_entries = 0;
+    st.configuration_table = core::ptr::null_mut();
+  } else {
+    //Box up the new table and put it in the system table. The old table (if any) will be dropped
+    //when old_cfg_table goes out of scope at the end of the function.
+    st.number_of_table_entries = new_table.len();
+    let new_table = new_table.to_vec_in(&EFI_RUNTIME_SERVICES_DATA_ALLOCATOR).into_boxed_slice();
+    st.configuration_table = Box::into_raw(new_table) as *mut ConfigurationTable;
+  }
+  //since we modified the system table, re-calculate CRC.
+  efi_st.checksum();
+
+  //signal the table guid as an event group
+  EVENT_DB.signal_group(vendor_guid);
+
+  Ok(())
+}
+
+eficall! {fn install_configuration_table (table_guid: *mut Guid, table: *mut c_void) -> Status {
+  if table_guid.is_null() {
+    return r_efi::efi::Status::INVALID_PARAMETER
+  }
+
+  let table_guid = unsafe {*table_guid};
+  let table = unsafe {table.as_mut()};
+
+  match core_install_configuration_table(table_guid, table) {
+    Err(err) => err,
+    Ok(()) => r_efi::efi::Status::SUCCESS,
+  }
+}}
+
 pub fn init_misc_boot_services_support(bs: &mut BootServices) {
   bs.calculate_crc32 = calculate_crc32;
+  bs.install_configuration_table = install_configuration_table;
 }
