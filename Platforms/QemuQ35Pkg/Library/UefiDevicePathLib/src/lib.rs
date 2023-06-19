7e5559572abbbbaa452a4628c6213dc71d3d1b9c
John Schock <joschock@microsoft.com>
2023-06-19 12:29:43 -0700

    Add documentation for UefiDevicePathLib

--- Platforms/QemuQ35Pkg/Library/UefiDevicePathLib/src/lib.rs
+++ Platforms/QemuQ35Pkg/Library/UefiDevicePathLib/src/lib.rs
@@ -1,10 +1,62 @@
+//! UEFI DevicePath utilities
+//!
+//! This library provides various utilities for interacting with UEFI device paths.
+//!
+//! ## License
+//!
+//! Copyright (C) Microsoft Corporation. All rights reserved.
+//!
+//! SPDX-License-Identifier: BSD-2-Clause-Patent
+//!
 #![no_std]
 #![feature(pointer_byte_offsets)]
 
 use core::ptr::slice_from_raw_parts;
 
-/// returns the number of nodes in the given device path and the size of the device path in bytes.
-/// node count and byte size both include the terminating end node.
+/// Returns the count of nodes and size (in bytes) of the given device path.
+///
+/// count and size outputs both include the terminating end node.
+///
+/// ## Safety
+///
+/// device_path input must be a valid pointer to a well-formed device path.
+///
+/// ## Examples
+///
+/// ```
+/// #![feature(pointer_byte_offsets)]
+/// use uefi_device_path_lib::device_path_node_count;
+/// use r_efi::protocols::device_path::{End, Hardware, TYPE_END, TYPE_HARDWARE};
+/// let device_path_bytes = [
+///   TYPE_HARDWARE,
+///   Hardware::SUBTYPE_PCI,
+///   0x6,  //length[0]
+///   0x0,  //length[1]
+///   0x0,  //func
+///   0x1C, //device
+///   TYPE_HARDWARE,
+///   Hardware::SUBTYPE_PCI,
+///   0x6, //length[0]
+///   0x0, //length[1]
+///   0x0, //func
+///   0x0, //device
+///   TYPE_HARDWARE,
+///   Hardware::SUBTYPE_PCI,
+///   0x6, //length[0]
+///   0x0, //length[1]
+///   0x2, //func
+///   0x0, //device
+///   TYPE_END,
+///   End::SUBTYPE_ENTIRE,
+///   0x4,  //length[0]
+///   0x00, //length[1]
+/// ];
+/// let device_path_ptr = device_path_bytes.as_ptr() as *const r_efi::protocols::device_path::Protocol;
+/// let (nodes, length) = device_path_node_count(device_path_ptr);
+/// assert_eq!(nodes, 4);
+/// assert_eq!(length, device_path_bytes.len());
+/// ```
+///
 pub fn device_path_node_count(device_path: *const r_efi::protocols::device_path::Protocol) -> (usize, usize) {
   let mut node_count = 0;
   let mut dev_path_size: usize = 0;
@@ -23,12 +75,116 @@ pub fn device_path_node_count(device_path: *const r_efi::protocols::device_path:
   (node_count, dev_path_size)
 }
 
+/// Computes the remaining device path and the number of nodes in common for two device paths.
+///
 /// if device path `a` is a prefix of or identical to device path `b`, result is Some(pointer to the portion of
 /// device path `b` that remains after removing device path `a`, nodes_in_common).
 /// if device path `a` is not a prefix of device path `b` (i.e. the first node in `a` that is different from
 /// `b` is not an end node), then the result is None.
 ///
 /// note: nodes_in_common does not count the terminating end node.
+///
+/// ## Safety
+///
+/// a and b inputs must be a valid pointers to well-formed device paths.
+///
+/// ## Examples
+///
+/// ```
+/// #![feature(pointer_byte_offsets)]
+/// use uefi_device_path_lib::{device_path_node_count, remaining_device_path};
+/// use core::mem::size_of;
+/// use r_efi::protocols::device_path::{End, Hardware, TYPE_END, TYPE_HARDWARE};
+/// let device_path_a_bytes = [
+///   TYPE_HARDWARE,
+///   Hardware::SUBTYPE_PCI,
+///   0x6,  //length[0]
+///   0x0,  //length[1]
+///   0x0,  //func
+///   0x1C, //device
+///   TYPE_HARDWARE,
+///   Hardware::SUBTYPE_PCI,
+///   0x6, //length[0]
+///   0x0, //length[1]
+///   0x0, //func
+///   0x0, //device
+///   TYPE_END,
+///   End::SUBTYPE_ENTIRE,
+///   0x4,  //length[0]
+///   0x00, //length[1]
+/// ];
+/// let device_path_a = device_path_a_bytes.as_ptr() as *const r_efi::protocols::device_path::Protocol;
+/// let device_path_b_bytes = [
+///   TYPE_HARDWARE,
+///   Hardware::SUBTYPE_PCI,
+///   0x6,  //length[0]
+///   0x0,  //length[1]
+///   0x0,  //func
+///   0x1C, //device
+///   TYPE_HARDWARE,
+///   Hardware::SUBTYPE_PCI,
+///   0x6, //length[0]
+///   0x0, //length[1]
+///   0x0, //func
+///   0x0, //device
+///   TYPE_HARDWARE,
+///   Hardware::SUBTYPE_PCI,
+///   0x6, //length[0]
+///   0x0, //length[1]
+///   0x2, //func
+///   0x0, //device
+///   TYPE_END,
+///   End::SUBTYPE_ENTIRE,
+///   0x4,  //length[0]
+///   0x00, //length[1]
+/// ];
+/// let device_path_b = device_path_b_bytes.as_ptr() as *const r_efi::protocols::device_path::Protocol;
+/// let device_path_c_bytes = [
+///   TYPE_HARDWARE,
+///   Hardware::SUBTYPE_PCI,
+///   0x6,  //length[0]
+///   0x0,  //length[1]
+///   0x0,  //func
+///   0x0A, //device
+///   TYPE_END,
+///   End::SUBTYPE_ENTIRE,
+///   0x4,  //length[0]
+///   0x00, //length[1]
+/// ];
+/// let device_path_c = device_path_c_bytes.as_ptr() as *const r_efi::protocols::device_path::Protocol;
+/// // a is a prefix of b.
+/// let result = remaining_device_path(device_path_a, device_path_b);
+/// assert!(result.is_some());
+/// let result = result.unwrap();
+/// // the remaining device path of b after going past the prefix in a should start at the size of a in bytes minus the size of the end node.
+/// let a_path_length = device_path_node_count(device_path_a);
+/// let offset = a_path_length.1 - size_of::<r_efi::protocols::device_path::End>();
+/// let offset = offset.try_into().unwrap();
+/// let expected_ptr =
+///   unsafe { device_path_b_bytes.as_ptr().byte_offset(offset) } as *const r_efi::protocols::device_path::Protocol;
+/// assert_eq!(result, (expected_ptr, a_path_length.0 - 1));
+
+/// //b is equal to b.
+/// let result = remaining_device_path(device_path_b, device_path_b);
+/// assert!(result.is_some());
+/// let result = result.unwrap();
+/// let b_path_length = device_path_node_count(device_path_b);
+/// let offset = b_path_length.1 - size_of::<r_efi::protocols::device_path::End>();
+/// let offset = offset.try_into().unwrap();
+/// let expected_ptr =
+///   unsafe { device_path_b_bytes.as_ptr().byte_offset(offset) } as *const r_efi::protocols::device_path::Protocol;
+/// assert_eq!(result, (expected_ptr, b_path_length.0 - 1));
+
+/// //a is not a prefix of c.
+/// let result = remaining_device_path(device_path_a, device_path_c);
+/// assert!(result.is_none());
+
+/// //b is not a prefix of a.
+/// let result = remaining_device_path(device_path_b, device_path_a);
+/// assert!(result.is_none());
+/// ```
+///
+
 pub fn remaining_device_path(
   a: *const r_efi::protocols::device_path::Protocol,
   b: *const r_efi::protocols::device_path::Protocol,
