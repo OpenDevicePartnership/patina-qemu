cd9cfd3d72350649ca7bb36416f54948e5e7a688
Michael Kubacki <michael.kubacki@microsoft.com>
2025-08-26 10:39:42 -0400

    QemuSbsaPkg/QemuVirtMemInfoLib: Produce V2 Resource Descriptor HOBs
    The Patina DXE Core will currently fail:
    
    ```
    ERROR - Failed to allocate root page for the page table page pool,
            retrying with normal allocation
    ERROR - Invalid memory attributes: MemoryAttributes(ExecuteProtect)
    ERROR - Failed to map memory region 0x10000000000 of length 0x101000
            with attributes 0x4000. Status: InvalidParameter
    ERROR - GCD and page table are out of sync. This is a critical error.
    ```
    
    Because cache attributes are not provided via V2 resource descriptor
    HOB for some ranges. This change produces V2 resource descriptor HOBs
    to provide the cache attributes.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- Platforms/QemuSbsaPkg/Library/QemuVirtMemInfoLib/QemuVirtMemInfoLib.c
+++ Platforms/QemuSbsaPkg/Library/QemuVirtMemInfoLib/QemuVirtMemInfoLib.c
@@ -93,11 +93,19 @@ ArmVirtGetMemoryMap (
   VirtualMemoryTable[0].Length       = *(UINT64 *)GET_GUID_HOB_DATA (MemorySizeHob);
   VirtualMemoryTable[0].Attributes   = ARM_MEMORY_REGION_ATTRIBUTE_WRITE_BACK;
 
-  BuildResourceDescriptorHob (
+  BuildResourceDescriptorV2 (
     EFI_RESOURCE_SYSTEM_MEMORY,
-    EfiConventionalMemory,
+    (EFI_RESOURCE_ATTRIBUTE_PRESENT | \
+     EFI_RESOURCE_ATTRIBUTE_INITIALIZED | \
+     EFI_RESOURCE_ATTRIBUTE_UNCACHEABLE | \
+     EFI_RESOURCE_ATTRIBUTE_WRITE_THROUGH_CACHEABLE | \
+     EFI_RESOURCE_ATTRIBUTE_WRITE_BACK_CACHEABLE | \
+     EFI_RESOURCE_ATTRIBUTE_TESTED \
+    ),
     VirtualMemoryTable[0].PhysicalBase,
-    VirtualMemoryTable[0].Length
+    VirtualMemoryTable[0].Length,
+    EFI_MEMORY_WB,
+    NULL
     );
 
   DEBUG ((
@@ -118,11 +126,18 @@ ArmVirtGetMemoryMap (
   VirtualMemoryTable[1].Length       = MACH_VIRT_PERIPH_SIZE;
   VirtualMemoryTable[1].Attributes   = ARM_MEMORY_REGION_ATTRIBUTE_DEVICE;
 
-  BuildResourceDescriptorHob (
+  BuildResourceDescriptorV2 (
     EFI_RESOURCE_MEMORY_MAPPED_IO,
-    EfiMemoryMappedIO,
+    (EFI_RESOURCE_ATTRIBUTE_PRESENT | \
+     EFI_RESOURCE_ATTRIBUTE_INITIALIZED | \
+     EFI_RESOURCE_ATTRIBUTE_UNCACHEABLE | \
+     EFI_RESOURCE_ATTRIBUTE_WRITE_COMBINEABLE | \
+     EFI_RESOURCE_ATTRIBUTE_TESTED \
+    ),
     VirtualMemoryTable[1].PhysicalBase,
-    VirtualMemoryTable[1].Length
+    VirtualMemoryTable[1].Length,
+    EFI_MEMORY_UC,
+    NULL
     );
 
   // Map the FV region as normal executable memory
@@ -131,11 +146,19 @@ ArmVirtGetMemoryMap (
   VirtualMemoryTable[2].Length       = FixedPcdGet32 (PcdFvSize);
   VirtualMemoryTable[2].Attributes   = ARM_MEMORY_REGION_ATTRIBUTE_WRITE_BACK_RO;
 
-  BuildResourceDescriptorHob (
+  BuildResourceDescriptorV2 (
     EFI_RESOURCE_SYSTEM_MEMORY,
-    EfiBootServicesCode,
+    (EFI_RESOURCE_ATTRIBUTE_PRESENT | \
+     EFI_RESOURCE_ATTRIBUTE_INITIALIZED | \
+     EFI_RESOURCE_ATTRIBUTE_UNCACHEABLE | \
+     EFI_RESOURCE_ATTRIBUTE_WRITE_THROUGH_CACHEABLE | \
+     EFI_RESOURCE_ATTRIBUTE_WRITE_BACK_CACHEABLE | \
+     EFI_RESOURCE_ATTRIBUTE_TESTED \
+    ),
     VirtualMemoryTable[2].PhysicalBase,
-    VirtualMemoryTable[2].Length
+    VirtualMemoryTable[2].Length,
+    EFI_MEMORY_WB,
+    NULL
     );
 
   // End of Table
