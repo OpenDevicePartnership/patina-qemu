86284300935abcb75bf0731633551dceb12c3692
Kun Qin <kuqin@microsoft.com>
2026-07-22 15:05:07 -0700

    Now it boots

--- Platforms/QemuArmVirtPkg/PlatformBuild.py
+++ Platforms/QemuArmVirtPkg/PlatformBuild.py
@@ -749,7 +749,7 @@ class PlatformBuilder(UefiBuilder, BuildSettingsManager):
         args += " DEBUG=" + str(1 if self.env.GetValue("TARGET").lower() == 'debug' else 0)
         args += " ENABLE_SME_FOR_SWD=0 ENABLE_SVE_FOR_SWD=0 ENABLE_SME_FOR_NS=0 ENABLE_SVE_FOR_NS=0"
         args += f" SPD=spmd SPMD_SPM_AT_SEL2=1 SP_LAYOUT_FILE={filename}"
-        args += " ENABLE_FEAT_HCX=1 HOB_LIST=1 TRANSFER_LIST=1 LOG_LEVEL=40 ENABLE_FEAT_MTE2=2 CTX_INCLUDE_PAUTH_REGS=1 CFLAGS='-Wno-error=c23-extensions' " # Features used by hypervisor
+        args += " ENABLE_FEAT_HCX=1 HOB_LIST=1 TRANSFER_LIST=1 LOG_LEVEL=40 ENABLE_FEAT_MTE2=2 CTX_INCLUDE_PAUTH_REGS=1 ENABLE_FEAT_TCR2=1 ENABLE_FEAT_S1PIE=1 CFLAGS='-Wno-error=c23-extensions' " # Features used by hypervisor
         # args += " FEATURE_DETECTION=1" # Enforces support for features enabled.
         args += f" BL32={str(haf_out / 'secure_qemu_aarch64_clang' / 'hafnium.bin')}"
         args += f" PRELOADED_BL33_BASE=0x04000000" # This value matches the QEMU_FLASH1_BASE in platform_def.h
